ea8a744b18cb5de4ae524a0c707d0b4c92f26a70
Yury Kovaliov <yurykavaliou86@gmail.com>
2020-10-25 14:42:21 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let num = [...(n.toString())].reverse().join('');
+  if(num[num.length-1]==='-') {
+    return num.slice(0, -1);
+  } else {
+    return num;
+  }
 }
